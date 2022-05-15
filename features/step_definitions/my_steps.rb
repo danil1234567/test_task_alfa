138ec8098664427a406d1d503192df519b547ad1
Danil <danmatv1997@gmail.com>
2022-05-15 11:14:57 +0300

    made comments for code

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -32,14 +32,14 @@ Then(/^Check elements$/) do
   end
   f_result.print(head1_result)
 
-  #Проверка ссылок (по стоимости недвижимости и по сумме кредита)
-  #По стоимости недвижимости
+  # Проверка ссылок (по стоимости недвижимости и по сумме кредита)
+  # По стоимости недвижимости
   by_cost_of_property =\
-  @browser.find_element(xpath: "//div[@class = 'calc-fright' ]//a").enabled? #инфо о доступности Элемента
-  by_cost_of_property_text = @browser.find_element(xpath: "//div[@class = 'calc-fright' ]//a").text #текст элемента
-  by_cost_of_property_expvalue = 'По стоимости недвижимости' #ожидаемый текст
+  @browser.find_element(xpath: "//div[@class = 'calc-fright' ]//a").enabled? # инфо о доступности Элемента
+  by_cost_of_property_text = @browser.find_element(xpath: "//div[@class = 'calc-fright' ]//a").text # текст элемента
+  by_cost_of_property_expvalue = 'По стоимости недвижимости' # ожидаемый текст
 
-  #Вывод результата:
+  # Вывод результата:
   if by_cost_of_property_text == by_cost_of_property_expvalue and by_cost_of_property == TRUE
     by_cost_of_property_result = "Ссылка 'по стоимости недвижимости' доступна\n"
   else
@@ -49,8 +49,19 @@ Then(/^Check elements$/) do
 
   #По сумме кредита:
   credit_sum = @browser.find_element(xpath: "//div[@class = 'calc-fright' ]//a[2]").enabled? #инфо о доступности Элемента
-  credit_sum_text = @browser.find_element(xpath: "//div[@class = 'calc-fright' ]//a[2]").text #текст элемента
   credit_sum_text_expvalue = "По сумме кредита"
+  credit_sum_text = @browser.find_element(xpath: "//div[@class = 'calc-fright' ]//a[2]").text #текст элемента
+
+  #Trash
+  # ========
+  # a = @browser.find_elements(xpath: "//div[@class = 'calc-fright' ]//a")
+  # for element in a
+  #   if element.text == credit_sum_text_expvalue
+  #     f_result.print(element.text)
+  #     f_result.print("\n")
+  #   end
+  # end
+  # =======
 
   #Вывод результата:
   if credit_sum == TRUE and credit_sum_text == credit_sum_text_expvalue
@@ -60,15 +71,13 @@ Then(/^Check elements$/) do
   end
   f_result.print(credit_sum_result)
 
-  #Проверка полей для заполнения данными
-  #стоимость недвижимости:
-  cost_of_property = @browser.find_element\
-(:xpath, "//div[@class = 'calc-frow type-x type-1'][1]//div[@class='calc-fleft']").enabled? #инфо о доступности Элемента
-  cost_of_property_text = @browser.find_element\
-(:xpath, "//div[@class = 'calc-frow type-x type-1'][1]//div[@class='calc-fleft']").text #текст элемента
-  cost_of_property_expvalue = "По стоимости недвижимости"
+  # Проверка полей для заполнения данными
+  # стоимость недвижимости:
+  cost_of_property = @browser.find_element(:xpath, "//div[@class = 'calc-frow type-x type-1'][1]//div[@class='calc-fleft']").enabled? #инфо о доступности Элемента
+  cost_of_property_text = @browser.find_element(:xpath, "//div[@class = 'calc-frow type-x type-1'][1]//div[@class='calc-fleft']").text #текст элемента
+  cost_of_property_expvalue = "Стоимость недвижимости" #Ожидаемое значение
 
-  #Вывод результата:
+  # Вывод результата:
   if cost_of_property == TRUE and cost_of_property_text == cost_of_property_expvalue
     cost_of_property_result = "Элемент 'стоимость недвижимости' доступен\n"
   else
@@ -76,13 +85,15 @@ Then(/^Check elements$/) do
   end
   f_result.print(cost_of_property_result)
 
+  # Первоначальный взнос
   first_pay = @browser.find_element(:xpath, "//div[@class = 'calc-frow type-x type-1'][2]//div[@class='calc-fleft']").enabled?
   if first_pay == TRUE
     f_result.print("Элемент 'первоначальный_взнос' доступен\n")
   else
-    f_result.print("Элемент 'первоначальный_взнос' недоступен\n")
+    f_result.print("Элемент 'первоначальный_взнос' не доступен\n")
   end
 
+  # Сумма кредита
   credit = @browser.find_element(:xpath, "//div[@class = 'calc-frow type-x type-1'][3]//div[@class='calc-fleft']").enabled?
   if credit == TRUE
     f_result.print("Элемент 'Сумма_кредита' доступен\n")
@@ -90,6 +101,7 @@ Then(/^Check elements$/) do
     f_result.print("Элемент 'Сумма_кредита' недоступен\n")
   end
 
+  # Срок кредита
   credit_time = @browser.find_element(:xpath, "//div[@class = 'calc-frow calc_type-x calc_type-1 calc_type-3']//div[@class = 'calc-fleft']").enabled?
   if credit_time == TRUE
     f_result.print("Элемент 'Срок_кредита' доступен\n")
@@ -97,6 +109,7 @@ Then(/^Check elements$/) do
     f_result.print("Элемент 'Срок_кредита' недоступен\n")
   end
 
+  # Процентная ставка
   credit_percent = @browser.find_element(:xpath, "//div[@class = 'calc-frow']//div[@class = 'calc-fleft']").enabled?
   if credit_percent == TRUE
     f_result.print("Элемент 'Процентная_ставка' доступен\n")
@@ -104,6 +117,7 @@ Then(/^Check elements$/) do
     f_result.print("Элемент 'Процентная_ставка' недоступен\n")
   end
 
+  # Тип ежемесячных платежей
   pay_type = @browser.find_element(:xpath, "//div[@class = 'calc-frow calc_type-x calc_type-1']//div[@class = 'calc-fleft']").enabled?
   if pay_type == TRUE
     f_result.print("Элемент 'Тип_ежемесячных_платежей' доступен\n")
@@ -114,44 +128,52 @@ Then(/^Check elements$/) do
 end
 
 Then(/^Check calculate$/) do
+
+  # Заполнить поле стоимость недвижимости
   cost_of_property = @browser.find_element(:xpath, "//div[@class = 'calc-frow type-x type-1']//div[@class = 'calc-fright']//div[@class = 'calc-input']//input[@name = 'cost']")
   cost_of_property.send_keys '12000000' #вставить в поле 12000000
 
+  #Выбрать из списка тип первоначального взноса: % от суммы
   start_sum_type = @browser.find_elements(:xpath, "//div[@class ='calc-frow type-x type-1']//div[@class ='calc-input']//select[@name = 'start_sum_type']//option")
   start_sum_type.each { |a| a.click if (a.attribute("value")== '2')}
 
+  #Установить значение первоначального взноса в 20%
   start_sum_per = @browser.find_element(:xpath, "//div[@class ='calc-frow type-x type-1']//div[@class ='calc-input']//input[@name='start_sum']")
   start_sum_per.send_keys '20'
 
+  # Todo: check_credit_sum and first_pay fields
 
-
-  #select_object = Select(start_sum_type)
-  #select_object.select_by(:value, "2")
-
+  #Установить срок кредита 20 лет
   credit_time = @browser.find_element(:xpath, "//div[@class ='calc-frow calc_type-x calc_type-1 calc_type-3']//div[@class ='calc-input']//input")
   credit_time.send_keys '20' #вставить в поле 20
 
+  #Генерируем процентную ставку
   rnd = Random.new
   credit_percent_value = rnd.rand(5..12)
 
+  #Заполняем поле с процентной ставкой сгенерированным значением
   credit_percent = @browser.find_element(:xpath, "//div[@class ='calc-frow']//div[@class ='calc-input']//input")
   credit_percent.send_keys credit_percent_value
 
+  #Проверка отмечен радиобаттон "Аннуитетные" и расчет
   radio_button_ann = @browser.find_element(:xpath, "//div[@class ='calc-fright']//div[@class ='mb-2']//input").selected?
   radio_button_dif = @browser.find_element(:xpath, "//div[@class ='calc-fright']//input[@value= '2']").selected?
   calculate_button = @browser.find_element(:xpath, "//div[@class ='calc-frow button-row']//input")
+
   if radio_button_dif == FALSE and radio_button_ann == TRUE
     calculate_button.click
   elsif radio_button_ann == FALSE
     @browser.find_element(:xpath, "//div[@class ='calc-fright']//div[@class ='mb-2']//input").click
     calculate_button.click
   end
-  sleep(5)
-  file1 = File.new('C:/Users/danma/RubymineProjects/untitled/data.txt', 'a:UTF-8')
-  file1.print("Указанные проценты #{credit_percent_value}\n")
 
+  # ожидание загрузки страницы
+  sleep(5)
+
+  # Извлекаем полученное значение ежемесячного платежа
   mothly_payment = @browser.find_element(:xpath, "//div[@class ='calc-result-value result-placeholder-monthlyPayment']").text
-  #расчет ежемесячного платежа для проверки:
+
+  #расчет ежемесячного платежа для проверки полученного значения:
   n = 240
   i = ((credit_percent_value.to_f)/12/100).to_f
   start_sum_per = 20
@@ -165,18 +187,13 @@ Then(/^Check calculate$/) do
   mothly_payment_exp = mothly_payment_exp.to_s
   mothly_payment_exp = mothly_payment_exp.sub(".", ",")
 
-  file1.print("расчетное значение #{mothly_payment_exp}\n")
-  file1.print("полученное значение #{mothly_payment}\n")
-
+  # вывод результата
   if mothly_payment_exp == mothly_payment
     f_result.print("Расчет выполнен верно #{mothly_payment_exp}, #{mothly_payment}\n")
   else
     f_result.print("Расчет выполнен неверно #{mothly_payment_exp}, #{mothly_payment}\n")
   end
 
-  file1.close
   f_result.close
-
-
 
 end
